a74db1917e5337ad55e6dcf15209e8c3a23c0fd6
Hacker <72150854+Future423@users.noreply.github.com>
2021-05-04 13:13:59 +0530

    Update weather.py

--- WeatherApp/weather.py
+++ WeatherApp/weather.py
@@ -25,18 +25,19 @@ def getWeather(canvas):
 
 
 canvas = tk.Tk()
-canvas.geometry("600x500")
+canvas.geometry("500x600")
 canvas.title("Weather App")
-f = ("poppins", 15, "bold")
-t = ("poppins", 35, "bold")
+canvas.configure(background="light cyan")
+f = ("Bradley Hand ITC",20, "bold")
+t = ("Baskerville Old Face", 35)
 
 textField = tk.Entry(canvas, justify='center', width = 20, font = t)
 textField.pack(pady = 20)
 textField.focus()
 textField.bind('<Return>', getWeather)
 
-label1 = tk.Label(canvas, font=t)
+label1 = tk.Label(canvas, font=t,bg='light cyan',pady=10)
 label1.pack()
-label2 = tk.Label(canvas, font=f)
+label2 = tk.Label(canvas,bg='light cyan', font=f)
 label2.pack()
 canvas.mainloop()
